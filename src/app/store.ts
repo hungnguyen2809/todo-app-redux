3c60407ae6ee048493e64edc9837387ae482a921
Hung Nguyen <hungnguyen99.nvh@gmail.com>
2021-12-19 13:25:10 +0700

    add redux-saga

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,17 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga';
 import rootReducers from './rootReducers';
+import rootSagas from './rootSagas';
 
-const composeEnhancers = composeWithDevTools();
+const sagaMiddleware = createSagaMiddleware();
+const middleware = applyMiddleware(sagaMiddleware);
+
+const composeEnhancers = composeWithDevTools(middleware);
 
 export const store = createStore(rootReducers, composeEnhancers);
+
+sagaMiddleware.run(rootSagas);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
